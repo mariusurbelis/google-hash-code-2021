7960da68980b05781be406807d777559d65c2964
Aleaf <aallgaier@dundee.ac.uk>
2021-02-25 19:00:30 +0000

    Input reader
    Takes in input and appends it into arraylist

--- GoogleHashCode2021.java
+++ GoogleHashCode2021.java
@@ -1,6 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 public class GoogleHashCode2021
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		BufferedReader reader;
+        List<String> input;
+
+        try{
+            reader = new BufferedReader(new FileReader("input/a.txt"));
+            input = new ArrayList<String>();
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                input.add(line);
+                line = reader.readLine();
+            }
+
+            System.out.println(input);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 	}
 }
