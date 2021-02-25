c5a1c91eeb95a286c0ee85a7ea0adcc08860650f
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-25 20:26:01 +0000

    Added traffic lights and queues

--- classes/Street.java
+++ classes/Street.java
@@ -1,9 +1,13 @@
 package classes;
 
+import java.util.Queue;
+
 public class Street {
     public int startsAt, endsAt;
     public String streetName;
     public int L;
+    public Queue<Car> cQueue;
+    public TrafficLights trafficLight;
     
     public Street(int _startsAt, int _endsAt, String _name, int _L){
         startsAt = _startsAt;
