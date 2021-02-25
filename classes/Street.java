063b061e86e1ffc09c0efd7f1434c55f5210c5a4
Aleaf <aallgaier@dundee.ac.uk>
2021-02-25 19:58:18 +0000

    Input parsing
    Parsing while making sure that line is diffferentiatedd

--- classes/Street.java
+++ classes/Street.java
@@ -1,5 +1,10 @@
 package classes;
 
 public class Street {
-
+    public int startsAt, endsAt;
+    
+    public Street(int _startsAt, int _endsAt){
+        startsAt = _startsAt;
+        endsAt = _endsAt;
+    }
 }
